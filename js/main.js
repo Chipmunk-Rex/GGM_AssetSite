0c45256dfd4696182c8bc20f23e4ff0d09f0bff3
Chipmunk-Rex <rexgood1005@gmail.com>
2024-08-09 11:43:18 +0900

    feat : asset modified date

--- js/main.js
+++ js/main.js
@@ -11,9 +11,9 @@ window.onload = function () {
         var name = columns[0];
         var modificationDate = columns[1];
         var link = columns[2];
+        var imgLink = 'https://ggm.gondr.net/image/users/380/profile/IMG_8779.jpg';
 
-        $(".main_assetList").append('<div class="main_assetBox"><div class="main_assetBox_imgbox"><img class="main_assetBox_img"src="https://ggm.gondr.net/image/users/397/profile/%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7%202024-06-02%20040653.png"alt=""></div><div class="main_assetBox_data"><span class="main_assetBox_data_title jua-regular">'+ name +'</span><div class="main_assetBox_data_buttons"><img class="main_assetBox_data_infoBtn button"src="https://cdn-icons-png.flaticon.com/128/5258/5258991.png" alt=""><a href='+ link +'><img class="main_assetBox_data_downloadBtn button"src="https://cdn-icons-png.flaticon.com/128/2550/2550337.png" alt=""></a></div></div></div>');
-
+        $(".main_assetList").append('<div class="main_assetBox"><div class="main_assetBox_imgbox"><img class="main_assetBox_img"src="' + imgLink +'"alt=""></div><div class="main_assetBox_data"><span class="main_assetBox_data_title font_jua-regular">'+ name +'</span>'+ '<div class="main_assetBox_data_date font_jua-regular">'+ modificationDate +'</div>' +'<div class="main_assetBox_data_buttons"><img class="main_assetBox_data_infoBtn button"src="https://cdn-icons-png.flaticon.com/128/5258/5258991.png" alt=""><a href='+ link +'><img class="main_assetBox_data_downloadBtn button"src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/512px-Google_Drive_icon_%282020%29.svg.png" alt=""></a></div></div></div>');
         console.log(columns);
       });
     });
